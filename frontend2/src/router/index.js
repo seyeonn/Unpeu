4a7f8bfb50a3c7f0b3a48c850aa54ffd8dd4d17a
seyeonn <ansse17@gmail.com>
2022-04-15 15:24:33 +0900

    Feat(FE): 프론트엔드 초기 셋팅
    Former-commit-id: d60a2437b2f4a4f3d614c84770930bd51e386c7c

---
+++ frontend2/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HelloWorld from "@/components/helloWorld.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: "/helloWorld",
+        name: "helloWorld",
+        component: HelloWorld
+    }
+]
+const router = new VueRouter({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
+  });
+  
+  export default router;
