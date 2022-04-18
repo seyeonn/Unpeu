aca367610e09ba9774fc5f970b840daad8be387b
seyeonn <ansse17@gmail.com>
2022-04-18 11:47:35 +0900

    Feat(FE): 메인 레이아웃 구성
    Former-commit-id: 945a558af161b5fd18c5c259b55d54765db8b3d2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,46 +1,7 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+  <div class="view">
+    <HelloWorld/>
+  </div>
 </template>
 
 <script>
@@ -58,3 +19,13 @@ export default {
   }),
 };
 </script>
+
+<style lang="css">
+.view {
+  margin-left: 120px;
+}
+body {
+  background-image: url("https://i.imgur.com/EI9xcZH.png");
+  background-size: cover;
+}
+</style>
