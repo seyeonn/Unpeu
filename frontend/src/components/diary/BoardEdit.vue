40433c938a8186d7842b94c4f39dc4b19c7f75c3
김정윤 <kimjy199@naver.com>
2022-04-20 17:30:03 +0900

    Design(FE): 다이어리 레이아웃 기본 구성 S06P31B201-69
    Former-commit-id: 3d75bc86634d08ca4e8b12e51b85a9df0ee0f300

---
+++ frontend/src/components/diary/BoardEdit.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <board-write type="edit" :value="form"></board-write>
+  </v-container>
+</template>
+
+<script>
+import BoardWrite from "@/components/diary/BoardWrite.vue";
+
+export default {
+  name: "BoardEdit",
+  data() {
+    return {
+      form: {
+        category: "2022_어른이날",
+        title: "제목",
+        contents:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc.",
+      },
+    };
+  },
+
+  components: {
+    BoardWrite,
+  },
+
+  mounted() {
+    // diaryId에 해당하는 Diary 객체 가져오기
+    // 가져온 값은 form에 저장
+    console.log(this.$route.params.diaryId);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ frontend/src/views/DiaryPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container fluid style="background:#DDEFA5">
+        <router-view></router-view>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'DiaryPage',
+}
+</script>
+
+<style lang="">
+    
+</style>
