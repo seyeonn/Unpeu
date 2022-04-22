cb809cbbfd1420f565e2cd2f6fff4fcb825231c5
recul12 <recul12@naver.com>
2022-04-22 10:28:38 +0900

    Feat(FE) : 선물관리 화면 레이아웃 구성
    Former-commit-id: fcbda8ec0f9ec3967b2c1ba8bd6c31b451c3ae74

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/views/mainPage.vue"
-
+import PresentPage from "@/views/PresentPage.vue";
+import PresentPayment from "@/components/PresentPayment.vue";
+import PresentManage from "@/components/PresentManage.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -9,6 +11,24 @@ const routes = [
         path: "/",
         name: "home",
         component: Home
+    },
+    {
+        path: "/present",
+        name: "Present",
+        component: PresentPage,
+        redirect : "/present/manage",
+        children:[
+            {
+                path: "manage",
+                name: "PresentManage",
+                component: PresentManage,
+            },
+            {
+                path: "payment",
+                name: "PresentPayment",
+                component : PresentPayment,
+            }
+        ]
     }
 ]
 const router = new VueRouter({
