59fa73a0cf1ae065229d0f5a9d212108e467585c
recul12 <recul12@naver.com>
2022-04-22 10:28:38 +0900

    Feat(FE) : 선물관리 화면 레이아웃 구성
    Former-commit-id: 332192ed21849e0c7f30ed7a0daf78b98e4c1ad6

---
+++ frontend/src/components/present/PresentRegister.vue
@@ -0,0 +1,55 @@
+<template>
+<v-form @submit.prevent="registPresent">
+<v-container fluid>
+  <v-row>
+    <v-col cols="5">
+        <v-file-input
+      :rules="rules"
+      accept="image/png, image/jpeg, image/bmp"
+      placeholder="이미지 등록하기"
+      prepend-icon="mdi-camera"
+    ></v-file-input>
+      </v-col>
+      <v-col cols="5">
+        <v-text-field
+          v-model="present"
+          label="Amount"
+          value="10.00"
+          prefix="₩"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="2">
+      <v-btn
+      rounded
+      color="primary"
+      dark
+      type="submit"
+    >
+      등록하기
+    </v-btn>
+      </v-col>
+
+  </v-row>
+</v-container>
+    </v-form>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      present: null,
+      rules: [
+        value => !value || value.size < 2000000 || 'Avatar size should be less than 2 MB!',
+      ],
+    }),
+    methods:{
+      registPresent(){
+        console.log(this.present);
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
