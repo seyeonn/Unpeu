3bed83422360be35510ce008ce201bb344622ed5
황보라 <bora6031@naver.com>
2022-04-25 20:23:16 +0900

    Feat(FE): 로그인 화면 추가 및 카카오 로그인 구현
    Former-commit-id: aa88e589b85b04fbade5104d1dee4da1631aff6f

---
+++ frontend/src/components/login/GoogleLogin.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>GoogleLogin</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "GoogleLogin",
+}
+</script>
---
+++ frontend/src/components/login/KakaoLogin.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>kakaoLogin</h1>
+    </div>
+</template>
+
+<script>
+import {loginUser} from '@/api/user.js';
+
+export default {
+  name: "KakaoLogin",
+  created() { 
+    if(this.searchParam('code')){
+        console.log(this.searchParam('code'))
+        //axios 로그인 진행 
+        loginUser(this.searchParam('code'),(res) => { 
+            localStorage.setItem("accessToken",res.data.accessToken)
+        },()=>{
+            console.log("fail")
+        });
+      this.$router.push("/") //메인 페이지로 이동
+    }
+    },
+  methods: {
+        searchParam(key) {
+            return new URLSearchParams(location.search).get(key);
+        }
+  }
+    
+}
+</script>
