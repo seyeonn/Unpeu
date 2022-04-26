b5fcde78e3473399b01f451c99a09e20362a0792
황보라 <bora6031@naver.com>
2022-04-26 10:17:04 +0900

    Feat(FE): 구글 로그인 axios 추가
    Former-commit-id: 8bfcbd2be69555d8b2318f853bfbc3ca05c79d34

--- frontend/src/components/login/GoogleLogin.vue
+++ frontend/src/components/login/GoogleLogin.vue
@@ -5,7 +5,29 @@
 </template>
 
 <script>
+import {googleLogin} from '@/api/user.js';
+
 export default {
   name: "GoogleLogin",
+  created() { 
+    if(this.searchParam('code')){
+        console.log(this.searchParam('code'))
+        alert(this.searchParam('code'))
+        //axios 로그인 진행 
+        googleLogin(this.searchParam('code'),
+        (res) => { 
+            localStorage.setItem("accessToken",res.data.accessToken)
+        },
+        ()=>{
+            console.log("fail")
+        });
+      this.$router.push("/") //메인 페이지로 이동
+    }
+    },
+  methods: {
+        searchParam(key) {
+            return new URLSearchParams(location.search).get(key);
+        }
+  }
 }
 </script>
