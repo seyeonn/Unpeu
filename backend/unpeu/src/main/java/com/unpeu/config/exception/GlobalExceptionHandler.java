b3642d17730c6f5fba23476e84d570a5558c43d0
손모은 <mo_ah@naver.com>
2022-04-26 11:46:02 +0900

    chore(BE) : Global Exception 하나로 합침
    Former-commit-id: 938c523c17d299c9709392115318d1127db8dbd6

--- backend/unpeu/src/main/java/com/unpeu/config/exception/GlobalExceptionHandler.java
+++ backend/unpeu/src/main/java/com/unpeu/config/exception/GlobalExceptionHandler.java
@@ -5,7 +5,11 @@ import static com.unpeu.config.exception.ErrorCode.*;
 import javax.validation.ConstraintViolationException;
 
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -35,5 +39,23 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
 		log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
 		return ErrorResponse.toResponseEntity(e.getErrorCode());
+	}
+
+
+	/**
+	 * @valid로 유효성 검사했을 때 발생하는 에러 처리
+	 */
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Object> methodValidationException(MethodArgumentNotValidException e) {
+		log.warn("MethodArgumentNotValidException 발생");
+		BindingResult bindingResult = e.getBindingResult();
+
+		StringBuilder builder = new StringBuilder();
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
+			builder.append(fieldError.getField()).append(" : ");
+			builder.append(fieldError.getDefaultMessage());
+			builder.append(", ");
+		}
+		return new ResponseEntity<>(builder.toString(), HttpStatus.BAD_REQUEST);
 	}
 }
