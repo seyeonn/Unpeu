58d4e3b54645b7b9d90702da0c7aed8e322ded85
김정윤 <kimjy199@naver.com>
2022-04-27 05:16:36 +0900

    Feat(BE): Comment REST API 추가
    Former-commit-id: 282fe6840749c6836b9c80ddb3ff906694b5d5e3

--- backend/unpeu/src/main/java/com/unpeu/domain/response/BoardInfoGetRes.java
+++ backend/unpeu/src/main/java/com/unpeu/domain/response/BoardInfoGetRes.java
@@ -3,30 +3,37 @@ package com.unpeu.domain.response;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.unpeu.domain.entity.Board;
-import com.unpeu.domain.entity.Comment;
 import io.swagger.annotations.ApiModel;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-@ApiModel("BoardGetRes")
+@ApiModel("BoardInfoGetRes")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class BoardGetRes {
+public class BoardInfoGetRes {
     private Long userId;
     private Long boardId;
+    private String title;
+    private String content;
     private String category;
 
     @JsonDeserialize(using = LocalDateTimeDeserializer.class) // 역직렬화 오류 해결
     private LocalDateTime createdAt;
 
-    public BoardGetRes(Board board) {
+    private List<CommentInfoGetRes> commentList;
+
+    public BoardInfoGetRes(Board board) {
         this.userId = board.getUser().getId();
         this.boardId = board.getBoardId();
+        this.title = board.getTitle();
+        this.content = board.getContent();
         this.category = board.getCategory();
         this.createdAt = board.getCreatedAt();
+        this.commentList = board.getComments().stream().map(CommentInfoGetRes::new).collect(Collectors.toList());
     }
-
 }
