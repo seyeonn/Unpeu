98b06f2ea50c671275e16968c1ff51ef284cb612
recul12 <recul12@naver.com>
2022-04-29 08:49:34 +0900

    Feat(FE) : 선물 수정, 삭제 기능 추가
    Former-commit-id: ebf251ef00dc556d878681dc04d8e56a69b025f6

--- frontend/src/components/present/PresentListSearch.vue
+++ frontend/src/components/present/PresentListSearch.vue
@@ -7,17 +7,51 @@
           <template v-for="(n,i) in columns">
             <template v-if="(+index+i-1)<=cardList.length">
               <v-col :key="i">
-                <v-card v-if="(+index+i)<cardList.length" class="card">
+                <v-hover v-slot="{ hover }">
+                <v-card :elevation="hover ? 12 : 2" :class="{ 'on-hover' : hover }"  v-if="(+index+i)<cardList.length" class="card">
                   <v-img
                     :src="API_BASE_URL+cardList[+index+i].presentImg"
                     aspect-ratio="0.8"
-                  ></v-img>
+                  >
+                    <v-expand-transition>
+                      <div
+                        v-if="hover"
+                        class="d-flex transition-fast-in-fast-out light-blue lighten-3 v-card--reveal display-3 white--text" style="height: 100%;"
+                      >
+                        <div class="align-self-center">
+                          <v-btn
+                            class="mx-2"
+                            fab
+                            color="yellow lighten-3"
+                            @click="openDialog(+index+i)"
+                            >
+                            <v-icon dark>
+                                mdi-pencil
+                            </v-icon>
+                          </v-btn>
+                          <v-btn
+                            class="mx-2"
+                            fab
+                            color="red lighten-3"  
+                            @click="remove(+index+i)"
+                          >
+                            <v-icon dark>
+                              mdi-close
+                            </v-icon>
+                          </v-btn>
+                        </div>
+                      </div>
+                    </v-expand-transition>
+                  </v-img>
                   <v-card-title primary-title>
-                    <div>
-                      <div> 가격 </div>
-                    </div>
+                    {{ cardList[+index+i].presentName }}
                   </v-card-title>
+                  <v-card-text>
+                    가격 : {{cardList[+index+i].presentPrice}}
+                  </v-card-text>
+                  
                 </v-card>
+                </v-hover>
               </v-col>
             </template>
           </template>
@@ -25,16 +59,28 @@
       </v-carousel-item>
     </template>
   </v-carousel>
+  <present-update-modal :openDialog="dialog" :present="selectedCardList" @close="closeDialog" />
   </div>
 </template>
 
 <script>
+import PresentUpdateModal from "@/components/present/PresentUpdateModal.vue";
 import {API_BASE_URL} from "../../config/index.js";
 import {mapActions, mapState} from "vuex";
 const presentStore="presentStore";
 export default {
+  components:{
+    PresentUpdateModal,
+  },
   data:()=>({
     cardList: [],
+    selectedCardList: null,
+    present:{
+      presentName:"",
+      presentImg:"",
+    },
+    cardIndex:0,
+    dialog:false,
     API_BASE_URL: API_BASE_URL,
   }),
   mounted(){
@@ -45,16 +91,24 @@ export default {
     columns(){
       // 카드 개수 3개로 고정
       return 3;
-    }
-
+    },
   },
   methods:{
-    ...mapActions(presentStore,["searchList"]),
+    ...mapActions(presentStore,["searchList","deletePresent"]),
     search(){
       this.searchList(1);
       this.cardList=this.presentList.Present;
       console.log(this.cardList);
-      this.cardListCount=Math.ceil(this.cardList.length/3);
+    },
+    openDialog(index){
+      this.dialog=true;
+      this.selectedCardList=this.cardList[index];
+    },
+    closeDialog(){
+      this.dialog=false;
+    },
+    remove(index){
+      this.deletePresent(this.cardList[index].presentId);
     }
   }
 }
@@ -63,6 +117,13 @@ export default {
 <style>
 .card {
   margin: 5px;
+  -webkit-transition: all .7s ease;
+    transition: all .7s ease;
+}
+
+.card:hover{
+  -webkit-transform: scale(1.05);
+    transform: scale(1.05);
 }
 
 .present-carousel{
@@ -79,4 +140,13 @@ export default {
         width:35px;
         top:calc(50% - 20px);
     }
+
+.v-card--reveal {
+    align-items: center;
+    bottom: 0;
+    justify-content: center;
+    opacity: .95;
+    position: absolute;
+    width: 100%;
+}
 </style>
