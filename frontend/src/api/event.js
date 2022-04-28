2cee2b203a004689fce086b7c6fdf45b0fb24bd8
손모은 <mo_ah@naver.com>
2022-04-28 15:11:47 +0900

    chore(FE) : baseurl에 api추가
    Former-commit-id: 302258c7d7841d243926e56b3e9306cb53a5b6ad

--- frontend/src/api/event.js
+++ frontend/src/api/event.js
@@ -3,7 +3,8 @@ import { tokenInstance } from "./index.js";
 const tokenApi = tokenInstance();
 
 function getMessage(success, fail){
-    tokenApi.get(`/api/message`)
+    console.log(localStorage.getItem("accessToken")) // 여기서 null 뜨면 1. login 하고 들어갔는지 확인 / 2. application-localstorage에서 accessToken있는지 확인
+    tokenApi.get(`/message`)
     .then(success)
     .catch(fail)
     console.log("실행됨")
--- frontend/src/api/present.js
+++ frontend/src/api/present.js
@@ -4,17 +4,17 @@ const tokenApi = Instance.tokenInstance();
 
 // 유저 선물 등록
 function register(present, success, fail){
-    tokenApi.post(`api/present`,present,{headers : {'Content-Type': 'multipart/form-data'}}).then(success).catch(fail);
+    tokenApi.post(`/present`,present,{headers : {'Content-Type': 'multipart/form-data'}}).then(success).catch(fail);
 }
 
 // 유저 선물 리스트 찾기
 function search(id, success, fail){
-    api.get(`api/present/${id}`).then(success).catch(fail);
+    api.get(`/present/${id}`).then(success).catch(fail);
 }
 
 // 메세지 전송
 function sendMessage(message, success, fail){
-    api.post(`api/present/message`, JSON.stringify(message)).then(success).catch(fail);
+    api.post(`/present/message`, JSON.stringify(message)).then(success).catch(fail);
 }
 
 export{
