d90ff5e8298275f54f55bae8bbe31d38442e9d58
recul12 <recul12@naver.com>
2022-04-29 08:49:34 +0900

    Feat(FE) : 선물 수정, 삭제 기능 추가
    Former-commit-id: 776272d0a8343bdadd65a1c30bb0e619a5e43692

--- frontend/src/api/present.js
+++ frontend/src/api/present.js
@@ -6,6 +6,18 @@ function register(present, success, fail){
     api.post(`api/present`,present,{headers : {'Content-Type': 'multipart/form-data'}}).then(success).catch(fail);
 }
 
+
+// 선물 수정
+function update(present, success, fail){
+    api.put(`api/present/${present.presentId}`,present.fd,{headers : {'Content-Type': 'multipart/form-data'}}).then(success).catch(fail);
+}
+
+
+// 선물 삭제
+function remove(presentId, success, fail){
+    api.delete(`api/present/${presentId}`).then(success).catch(fail);
+}
+
 // 유저 선물 리스트 찾기
 function search(id, success, fail){
     api.get(`api/present/${id}`).then(success).catch(fail);
@@ -16,8 +28,11 @@ function sendMessage(message, success, fail){
     api.post(`api/present/message`, JSON.stringify(message)).then(success).catch(fail);
 }
 
+
 export{
     register,
+    update,
+    remove,
     search,
     sendMessage,
 }
