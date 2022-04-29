843681237e42408b704b49c711178a894633930e
김정윤 <kimjy199@naver.com>
2022-04-30 04:29:14 +0900

    Feat(FE): CICD를 위한 환경설정 #S06P31B201-134
    Former-commit-id: 5b6f77a4613f898bad0adccf94bd1f5778cd9906

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -2,10 +2,7 @@
  * Enpeu의 개발 환경 상수들입니다.
  */
 
-const API_BASE_URL = "http://localhost:8080";
-const FRONT_URL= "http://localhost:8081";
+const API_BASE_URL = "http://k6b201.p.ssafy.io:8080";
+const FRONT_URL = "http://k6b201.p.ssafy.io";
 
-export {
-    API_BASE_URL,
-    FRONT_URL
-}
+export { API_BASE_URL, FRONT_URL };
