1086ee7a74ef331e80823019f976317e77f500f9
김정윤 <kimjy199@naver.com>
2022-05-02 03:08:55 +0900

    Feat(FE): Comment REST API 연결 및 레이아웃 수정 #S06P31B201-130 #S06P31B201-132
    Former-commit-id: c1910f63af14b85e087bfad95a141e7c7023ee82

--- frontend/src/components/diary/comment/Comment.vue
+++ frontend/src/components/diary/comment/Comment.vue
@@ -1,53 +1,101 @@
 <template>
-  <v-container>
-    <v-simple-table dense>
-          <tbody v-if="commentList.length">
-            <tr
-              v-for="item in commentList"
-              :key="item.commentId"
-            >
-              <td><v-icon>mdi-account</v-icon>{{ item.writer }}</td>
-              <td>{{ item.contents }}</td>
-              <td>{{ item.createdAt }}</td>
-            </tr>
-          </tbody>
-        </v-simple-table>
-  </v-container>
+  <v-simple-table dense>
+    <tbody v-if="commentList.length">
+      <tr v-for="item in commentList" :key="item.commentId">
+        <td style="width: 100px;" class="pa-0">
+          <v-icon small>mdi-account</v-icon>{{ item.writer }}
+        </td>
+
+        <td class="content pa-0">{{ item.content }}</td>
+
+        <td style="width: 110px;" class="pa-0 text-center">
+          {{ item.createdAt }}
+        </td>
+
+        <td style="width: 23px;" class="pa-0">
+          <v-btn @click="editComment(item)" icon x-small color="grey"
+            ><v-icon>mdi-pencil</v-icon></v-btn
+          >
+        </td>
+
+        <td style="width: 20px;" class="pa-0">
+          <v-btn @click="deleteComment(item)" icon x-small color="red"
+            ><v-icon>mdi-close</v-icon></v-btn
+          >
+        </td>
+      </tr>
+    </tbody>
+  </v-simple-table>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+const diaryStore = "diaryStore";
+
 export default {
-  name: 'CommentList',
+  name: "CommentList",
 
   data() {
-    return {
-      commentList: [
-        {
-          commentId: 1,
-          writer: "test1",
-          contents: "test중입니다",
-          createdAt: "2022-04-19 13:07:58",
-        },
-        {
-          commentId: 2,
-          writer: "test2",
-          contents: "test중입니다",
-          createdAt: "2022-04-20 13:07:58",
-        },
-      ],
-    };
+    return {};
   },
 
-  mounted() {
-    
+  props: {
+    commentList: {
+      type: Array,
+    },
+  },
+
+  computed: {
+    // 이름 지정해서 getters 가져오기
+    ...mapGetters(diaryStore, {
+      nonUser: "GET_COMMENT_DETAIL",
+    }),
   },
 
   methods: {
-    
+    ...mapActions(diaryStore, [
+      "ACT_EDIT_COMMENT",
+      "ACT_DELETE_COMMENT",
+      "ACT_SETTING_COMMENT",
+    ]),
+
+    /* 댓글 수정 요청하기 */
+    editComment(commentInfo) {
+      console.log("edit comment");
+      var input = prompt("비밀번호를 입력하세요");
+
+      if (input == commentInfo.password) {
+        this.ACT_SETTING_COMMENT({
+          editFlag: true,
+          commentInfo: commentInfo,
+        });
+      } else {
+        alert("비밀번호가 틀렸습니다.");
+      }
+    },
+
+    /* 댓글 삭제하기 */
+    deleteComment(commentInfo) {
+      console.log("delete comment");
+      var input = prompt("비밀번호를 입력하세요");
+
+      if (input == commentInfo.password) {
+        this.ACT_DELETE_COMMENT({
+          commentId: commentInfo.commentId,
+          password: commentInfo.password,
+        });
+        this.$router.go();
+      } else {
+        alert("비밀번호가 틀렸습니다.");
+      }
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-
+// v-html없이 줄바꿈
+.content {
+  white-space: pre-line
+}
 </style>
