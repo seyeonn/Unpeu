7009b31c867f034733054ec12f1ece3919d362d1
손모은 <mo_ah@naver.com>
2022-05-01 13:13:01 +0900

    Chore(FE) : getMessage url변경
    Former-commit-id: 660e25b56d68541472b05beb27caeab1d007cc42

--- frontend/src/components/EventRoom.vue
+++ frontend/src/components/EventRoom.vue
@@ -81,10 +81,11 @@ export default {
             perPage: 10,
             // 현재 페이지
             currentPage: 1,
+            userId: this.$store.state.userStore.user.id, //url or param에서 userId를 뽑게 되면 바뀌어야 함.
         }
     },
     mounted() {
-        getMessage(
+        getMessage(this.userId,
             (res) => {
                 console.log(res.data.Message);
                 this.messages = res.data.Message;
