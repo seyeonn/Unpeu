f4047bace3f3e34475962b003109f40390334359
황보라 <bora6031@naver.com>
2022-05-02 23:23:13 +0900

    Chore(FE): 모달 메세지 수정 및 모달 띄우는 조건 추가
    Former-commit-id: c2ab0db6f2e8a3c1281608f931bcc82d18d4098d

--- frontend/src/components/login/GoogleLogin.vue
+++ frontend/src/components/login/GoogleLogin.vue
@@ -20,7 +20,7 @@ export default {
           //로그인 성공시 token 으로 userid값 가져오기
             getUserIdUseToken(token,(res)=>{
 
-              if(!res.data.User.userEmail&&!res.data.User.userBirth){//userEmail이 없으면 회원가입 방금한 사람
+              if(!res.data.User.userEmail||!res.data.User.userBirth){//userEmail이나 birth가 하나라도 없으면 회원가입 방금한 사람
                 this.setUserBirth(token,res.data.User)//생일이랑 email 수정-> 수정 성공시 accesstoken+ user정보 저장하고 마이페이지 이동 
                  
               }else{//이전에 생일이랑 email 작성한사람 이면 바로 accesstoken이랑 user정보 저장 후 마이페이지 이동
@@ -48,6 +48,7 @@ export default {
             title: '이메일과 생일을 입력해주세요!',
             html: '<input placeholder="이메일을 입력해주세요" id="swal-input1" class="swal2-input">' +
             '<input placeholder="생일을 입력해주세요" class="swal2-input" id="expiry-date">',
+            inputLabel: '여러분의 이메일과 생일을 입력해주세요. 드디어 마이페이지가 생성됩니다 :)',
             stopKeydownPropagation: false,
             focusConfirm: true,
             showCancelButton: true,
