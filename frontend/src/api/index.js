87d0fe4e0d505999a7cdde0f42002e2a4ccea5a9
김정윤 <kimjy199@naver.com>
2022-05-04 11:25:39 +0900

    Chore(FE): Local URL -> SERVER URL로 변경
    Former-commit-id: e8191e02a922faf8146f7263494437f99bf58158

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { API_SERVER_URL } from "../config";
+import { API_BASE_URL } from "../config";
 
 function createInstance() {
   const instance = axios.create({
-    baseURL: API_SERVER_URL+"/api",
+    baseURL: API_BASE_URL+"/api",
     headers: {
       "Content-Type": "application/json",
     },
