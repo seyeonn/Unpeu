8a25fc590e2a9b6e26ceb7a1271ae9f0b74d07ac
손모은 <mo_ah@naver.com>
2022-05-04 14:44:54 +0900

    Feat(FE) : 사용방법 이미지 추가
    Former-commit-id: fa9ad4acd667ab529e7426c21bd226450ca2c275

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -5,9 +5,11 @@
 const API_BASE_URL = "http://localhost:8080";
 const FRONT_URL= "http://localhost:8081";
 const KAKAO_API_KEY = "85706c8831cc3c166937be3d85a3d8a1";
+const EVENT_URL = "https://www.naver.com/";
 
 export {
     API_BASE_URL,
     FRONT_URL,
-    KAKAO_API_KEY
+    KAKAO_API_KEY,
+    EVENT_URL
 }
