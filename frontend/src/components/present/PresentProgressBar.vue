b5bc854652f86352eda392ec9a210351122c5db5
recul12 <recul12@naver.com>
2022-05-12 10:40:10 +0900

    Chore(FE) : 프로그래스바 수정
    Former-commit-id: b0ea21b7644a9d1f831c8da0c34693a5d61f90c0

---
+++ frontend/src/components/present/PresentProgressBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-progress-linear
+    v-model="computedPrice"
+    color="amber"
+    height="15"
+    >
+    <template v-slot:default="{ value }">
+      <strong>{{ Math.ceil(value) }}%</strong>
+    </template>
+  </v-progress-linear>
+</template>
+
+<script>
+export default {
+  props: ["presentPrice", "receivedPrice"],
+  data(){
+    return{
+      computedPrice: this.receivedPrice/this.presentPrice*100,
+    }
+  },
+  watch:{
+    presentPrice(){
+      this.computedPrice=this.receivedPrice/this.presentPrice*100
+    },
+    receivedPrice(){
+      this.computedPrice=this.receivedPrice/this.presentPrice*100
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
