76192ebe1b9d30d9ebc7646b61ef8ac214a794f1
손모은 <mo_ah@naver.com>
2022-05-16 17:28:21 +0900

    Chore(FE) : mypage로이동시 컨셉변경 안되는 에러 처리
    Former-commit-id: 06988d8606d6de973fea2f10ffd32831d706bf79

--- frontend/src/store/modules/userStore.js
+++ frontend/src/store/modules/userStore.js
@@ -1,4 +1,6 @@
 import * as api from "@/api/user";
+import router from "../../router";
+import dayjs from "dayjs";
 export const userStore = {
   namespaced: true,
 
@@ -39,11 +41,14 @@ export const userStore = {
     MU_CUR_USER_PERMISSION(state, permission) {
       state.currentUser.permission = permission;
     },
-    
-    MU_CUR_USER_CONCEPT(state, category, selectedDate) {
-      state.currentUser.category = category;
-      state.currentUser.selectedDate = selectedDate;
-      console.log("MU_CUR_USER_CONCEPT : ", state.currentUser);
+
+    MU_CUR_USER_CONCEPT(state, data) {
+      state.currentUser.category = data.category;
+      state.currentUser.selectedDate = data.selectedDate;
+      // console.log("MU_CUR_USER_CONCEPT : ", state.currentUser);
+    },
+    MU_USER_DETAIL(state, user) {
+      state.user = user;
     },
   },
 
@@ -54,11 +59,13 @@ export const userStore = {
         data,
         (response) => {
           console.log("AC_UPDATE_CONCEPT : ", response.data);
-          commit(
-            "MU_CUR_USER_CONCEPT",
-            response.data.User.category,
-            response.data.User.selectedDate
-          );
+          const data = {
+            category: response.data.User.category,
+            selectedDate: dayjs(
+              response.data.User.selectedDate.join("-")
+            ).format("YYYY-MM-DD"),
+          };
+          commit("MU_CUR_USER_CONCEPT", data);
           return response.data;
         },
         (err) => {
@@ -66,6 +73,119 @@ export const userStore = {
           return false;
         }
       );
+    },
+
+    AC_USER_DETAIL({ commit, dispatch }, userId) {
+      console.log("AC_USER_DETAIL-호출");
+      api.getUserDetail(
+        userId,
+        (response) => {
+          console.log("AC_USER_DETAIL", response.data);
+          
+          let today = null;
+          if (response.data.User.selectedDate != null) {
+            today = dayjs(response.data.User.selectedDate.join("-")).format(
+              "YYYY-MM-DD"
+            );
+          }
+          const data = {
+            category: response.data.User.category,
+            selectedDate: today,
+          };
+          commit("MU_CUR_USER_ID", userId);
+          commit("MU_CUR_USER_CONCEPT", data);
+          dispatch("AC_USER_PERMISSION", userId);
+          dispatch("AC_CHECK_CONCEPT", userId);
+        },
+        (err) => {
+          console.log(err);
+          if (err.response.status == 400) router.push({ name: "*" });
+        }
+      );
+    },
+    AC_USER_PERMISSION({ commit, dispatch, state }, userId) {
+      console.log("AC_USER_PERMISSION-호출");
+      if (localStorage.getItem("accessToken")) {
+        dispatch("AC_USER_TOKEN_DETAIL").then(() => {
+          //console.log("AC_USER_PERMISSION",state.user)
+
+          let compareId = state.user.id;
+          if (compareId != userId) {
+            commit("MU_CUR_USER_PERMISSION", 1); //login user
+          } else {
+            commit("MU_CUR_USER_PERMISSION", 0); //user
+          }
+          console.log("AC_USER_PERMISSION", state.currentUser);
+        });
+      } else {
+        commit("MU_CUR_USER_PERMISSION", 2); //guest
+      }
+    },
+    AC_USER_TOKEN_DETAIL({ commit }) {
+      console.log("AC_USER_TOKEN_DETAIL- 호출");
+      api.getUserDetailUseToken(
+        localStorage.getItem("accessToken"),
+        (response) => {
+          console.log("AC_USER_TOKEN_DETAIL", response.data);
+          commit("MU_USER_DETAIL", response.data.User);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    },
+    AC_CHECK_CONCEPT({  state }) {
+      console.log("AC_CHECK_CONCEPT-호출")
+      let today = dayjs().format("YYYY-MM-DD");
+      let concept = state.currentUser.category;
+      let selectedDate = state.currentUser.selectedDate;
+      console.log("selectedDate : ", selectedDate);
+      console.log("today : ", this.today);
+      console.log("CheckConcept-Concept : ", concept);
+      switch (concept) {
+        case "default":
+          if (selectedDate >= today) {
+            document.documentElement.setAttribute(
+              "color-theme",
+              "default-open"
+            );
+          } else {
+            document.documentElement.setAttribute(
+              "color-theme",
+              "default-close"
+            );
+          }
+          break;
+        case "birthday":
+          if (selectedDate >= today) {
+            document.documentElement.setAttribute(
+              "color-theme",
+              "birthday-open"
+            );
+          } else {
+            document.documentElement.setAttribute(
+              "color-theme",
+              "birthday-close"
+            );
+          }
+          break;
+        case "children":
+          if (selectedDate >= today) {
+            document.documentElement.setAttribute(
+              "color-theme",
+              "children-open"
+            );
+          } else {
+            document.documentElement.setAttribute(
+              "color-theme",
+              "children-close"
+            );
+          }
+          break;
+        default:
+          console.log(concept);
+          console.log("어떤 값인지 파악이 되지 않습니다.");
+      }
     },
   },
 };
